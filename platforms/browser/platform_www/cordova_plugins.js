2c8f09b44a6f414abda39d1b906b217c5e802397
elanthirayan <elanthirayan.m@axinovate.com>
2016-10-18 11:33:49 +0530

    push plugin

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -7,12 +7,29 @@ module.exports = [
         "clobbers": [
             "PushbotsPlugin"
         ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/browser/push.js",
+        "id": "phonegap-plugin-push.BrowserPush",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "pushbots-cordova-plugin": "1.3.9"
+    "pushbots-cordova-plugin": "1.3.9",
+    "phonegap-plugin-push": "1.8.3"
 }
 // BOTTOM OF METADATA
 });
