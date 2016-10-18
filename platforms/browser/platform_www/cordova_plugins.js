b6441a4d2c8bb465d399d82222c0e5a08f0e737b
elanthirayan <elanthirayan.m@axinovate.com>
2016-10-18 13:25:22 +0530

    gcmpush

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,7 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-gcmpushplugin/www/gpp.js",
+        "id": "cordova-plugin-gcmpushplugin.GCMPushPlugin",
+        "pluginId": "cordova-plugin-gcmpushplugin",
+        "clobbers": [
+            "GcmPushPlugin"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-gcmpushplugin": "1.2.0"
+}
 // BOTTOM OF METADATA
 });
