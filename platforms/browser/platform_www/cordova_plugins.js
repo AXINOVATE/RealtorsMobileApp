102eeac2aadf7c7a063fa83bb89e5c82590c3a44
elanthirayan <elanthirayan.m@axinovate.com>
2016-10-18 13:29:36 +0530

    removed gcm

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,18 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/cordova-plugin-gcmpushplugin/www/gpp.js",
-        "id": "cordova-plugin-gcmpushplugin.GCMPushPlugin",
-        "pluginId": "cordova-plugin-gcmpushplugin",
-        "clobbers": [
-            "GcmPushPlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-plugin-gcmpushplugin": "1.2.0"
-}
+{}
 // BOTTOM OF METADATA
 });
