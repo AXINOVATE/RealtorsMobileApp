9ac76267a126a2f9e80f20f4726c85e88e905144
elanthirayan <elanthirayan.m@axinovate.com>
2016-10-18 14:15:53 +0530

    removed pushbots

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,18 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/pushbots-cordova-plugin/www/pushbots.js",
-        "id": "pushbots-cordova-plugin.PushbotsPlugin",
-        "pluginId": "pushbots-cordova-plugin",
-        "clobbers": [
-            "PushbotsPlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "pushbots-cordova-plugin": "1.3.9"
-}
+{}
 // BOTTOM OF METADATA
 });
